83db696fe506af7704992ae8d8345a0628e961c9
Antero Marjamäki <ehtycs.github@gmail.com>
2022-03-04 15:40:25 +0200

    Added set/get operations for petsc Vec/Mat objects

--- api/python/src/user_wrapper.cpp
+++ api/python/src/user_wrapper.cpp
@@ -6,11 +6,10 @@ namespace py = pybind11;
 
 void init_user(py::module &m)
 {
-    py::class_<myexp>(m, "myexp")
-    
-        .def(py::init<>())
-
-        .def("yeet", &myexp::yeet, py::arg("name"))
-    ;
+    m.def("getvecdata", &getvecdata, py::arg("slvector"));
+    m.def("setvecdata", &setvecdata, py::arg("slvector"), py::arg("values"));
+    m.def("getcsrdata", &getcsrdata, py::arg("slmat"));
+    m.def("setcsrdata", &setcsrdata, py::arg("slmat"), 
+          py::arg("rowi"), py::arg("coli"), py::arg("vals"));
 }
 
